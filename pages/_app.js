28e0a4136f4e7cb8e711710f37de24b01e13a552
Akash Malbari <akashmalbari@gmail.com>
2026-03-27 10:45:41 -0400

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
+import { useEffect } from 'react';
 import Head from 'next/head';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
 import '../styles/globals.css';
 
+const GA_MEASUREMENT_ID = 'G-5HK286BQ15';
+
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (typeof window.gtag === 'function') {
+        window.gtag('config', GA_MEASUREMENT_ID, {
+          page_path: url,
+        });
+      }
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
@@ -9,6 +31,23 @@ export default function App({ Component, pageProps }) {
         <link rel="shortcut icon" href="/favicon.svg" />
         <meta name="theme-color" content="#0a0a0a" />
       </Head>
+
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga4-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          window.gtag = gtag;
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+
       <Component {...pageProps} />
     </>
   );
